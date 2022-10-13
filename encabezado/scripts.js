0b269d6de6984746562f2fcf47e0e53eb378d0a2
mayc22 <114185488+mayc22@users.noreply.github.com>
2022-10-13 16:47:16 -0500

    Add files via upload

---
+++ encabezado/scripts.js
@@ -0,0 +1,23 @@
+/*jQuery('document').ready(function($){
+
+    var menuBtn = $('.menu-icon');
+    var menu= $('.navigation ul');
+
+    menuBtn.click(function(){
+
+        if( menu.hasClass('show')){
+
+          menu.removeClass('show');
+        } else{
+
+            menu.addClass('show');
+        }
+        
+        
+        
+
+        
+
+    });
+
+}); */
