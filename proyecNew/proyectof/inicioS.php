ca2b0c26bb3db7a6f265d7ce1379a11febae62c2
mayc22 <114185488+mayc22@users.noreply.github.com>
2022-11-08 23:07:55 -0500

    Add files via upload
    formualrios: inicio, registro

---
+++ proyecNew/proyectof/inicioS.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../proyectof/css/inicioS.css"> 
+    <link rel="shortcut icon" href="../proyectof/img/hexagono.png">
+    <title>Iniciar sesión</title>
+</head>
+<body>
+    <form action="" method="POST" id="form1">
+        <div class="form">
+            <h1>Iniciar sesión</h1>
+            <div class="grupo">
+                <input type="text" name="" id="name" required><span class="barra"></span>
+                <label for="">Nombre de usuario</label>
+            </div>
+             <div class="grupo">
+                <input type="password" name="" id="password" required><span class="barra"></span>
+                <label for="">Contraseña</label>
+            </div>
+            <button type="submit">Iniciar sesión</button>
+        </div>
+    </form>
+    <script src="ASSETS/JS/validFormsMain/inicioS.js"></script>
+
+</body>
+</html>
